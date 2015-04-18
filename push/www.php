c0f3705de59ada3dbc5ee3f594e4c83fd618acb8
Ben Pilgrim <ben@pilgrim.me.uk>
2015-04-18 20:18:10 +0100

    Remove old auto-deploy

--- push/www.php
+++
@@ -1,4 +0,0 @@
-<?php
-shell_exec("/usr/bin/git --work-tree=/home/terra/www/www.terragaming.co.uk --git-dir=/home/terra/www/www.terragaming.co.uk/.git pull origin master 2>&1");
-shell_exec("/usr/bin/git --work-tree=/home/terra/www/www.terragaming.co.uk --git-dir=/home/terra/www/www.terragaming.co.uk/.git checkout -f master 2>&1");
-?>
